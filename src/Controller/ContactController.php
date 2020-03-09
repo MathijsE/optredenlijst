2b3a57183623e9300da76c13b0758e90e7999536
MathijsE <48790027+MathijsE@users.noreply.github.com>
2020-03-09 13:20:08 +0100

    contact form

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request)
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            dump($contactFormData);
+        }
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
